0d37cf25e8481464716661c698abe8a2444a6635
Deja S <deja28.14@gmail.com>
2020-10-11 04:04:18 -0400

    Modified test data, added more structure to settings page, and added setting's route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'detail',
     loadChildren: () => import('./detail/detail.module').then( m => m.DetailPageModule)
   },
+  {
+    path: 'settings',
+    loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule)
+  },
 ];
 
 @NgModule({
--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as data from '../../assets/test_data.json';
 
 @Component({
   selector: 'app-settings',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.page.scss'],
 })
 export class SettingsPage implements OnInit {
+
+  private user = data['user_data'];
+  private categories = data['categories'];
 
   constructor() { }
 
